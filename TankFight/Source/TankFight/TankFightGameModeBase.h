23125bc6c7d5071b9694165093a2b410b2e885c4
DESKTOP-C9UE8G8\suvar <suvarna.adit@gmail.com>
2018-03-01 17:31:48 -0500

    added unreal files

---
+++ TankFight/Source/TankFight/TankFightGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TankFightGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKFIGHT_API ATankFightGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
